c681cb2db502edadfef2cb0e8efb6dacddb37aa4
Rahul T <quickshifter@Rahuls-MacBook-Pro.local>
2023-03-25 21:30:47 +0530

    Code Complete

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { TextInput, View, Text, StyleSheet } from 'react-native';
+import { TextInput, View, Text, StyleSheet, FlatList, Image } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import Picks from "../components/Picks"
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5"
+import axios from 'axios';
+import { TouchableOpacity } from 'react-native';
+import { moderateScale } from 'react-native-size-matters';
 
 
 const Search = () => {
     const [text, setText] = useState('');
     const [result, setResult] = useState(false)
+    const [books, setBooks] = useState([]);
+
 
     const handleKeyPress = () => {
         setResult(true)
         // Search()
     };
+    useEffect(() => {
+        axios.get(`https://www.googleapis.com/books/v1/volumes?q=${text}`)
+            .then(response => setBooks(response.data.items))
+            .catch(error => setResult(false));
+    }, [text]);
 
     return (
         <>
@@ -29,11 +38,51 @@ const Search = () => {
                     />
                 </View>
             </View>
-            {result &&
-                <ScrollView>
-                    <Picks horizontal={false} name={` Resuls For "${text}" `} bookname={text} size={325} images={1} />
-                </ScrollView>
-            }
+            <ScrollView>
+                <View>
+                    {
+                        result &&
+                        <Text style={{ color: "black", padding: 10, height: 45, width: 150, }}>
+                            {`Results for "${text}"`}
+                        </Text>
+                    }
+                    <FlatList
+                        data={books}
+                        keyExtractor={item => item.id}
+                        style={{ marginTop: 5 }}
+                        renderItem={({ item }) =>
+                            <>
+                                <TouchableOpacity onPress={() => { console.log(item), setResult(false) }}>
+                                    <View style={{
+                                        display: "flex",
+                                        justifyContent: "space-evenly",
+                                        flexDirection: "column",
+                                        height: moderateScale(125),
+                                        width: moderateScale(300),
+                                        margin: 30,
+                                        backgroundColor: "#ddd",
+                                        padding: 15,
+                                    }}>
+                                        <View style={[{
+                                            display: "flex", margin: 10, justifyContent: "center",
+                                            alignItems: "center"
+                                        }]}>
+                                            <Image source={require("../assets/books2.jpg")} style={{ width: 75, height: 75, borderRadius: 50 }} />
+                                        </View>
+                                        <View style={styles.text}>
+                                            <Text numberOfLines={1} style={{ fontSize: 10, color: "#000", fontSize: 15 }}>{item.volumeInfo.title}</Text>
+                                        </View>
+                                    </View>
+                                </TouchableOpacity>
+                                <View>
+                                    <Text style={{ fontSize: 10 }}>{books.volumeInfo}</Text>
+                                </View>
+                            </>
+                        }
+                    />
+                </View>
+            </ScrollView>
+
         </>
     );
 };
@@ -43,12 +92,19 @@ export default Search;
 
 const styles = StyleSheet.create({
     container: {
-        // backgroundColor: "black",
-        borderColor: '#000000',
         borderBottomWidth: 1,
-        margin: 20,
+        margin: 10,
         display: "flex",
         flexDirection: "row",
-        backgroundColor: "#ddd"
+        backgroundColor: "#ddd",
+        justifyContent: "center",
+        alignItems: "center"
+
+    }
+    , text: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 19
     }
 })
